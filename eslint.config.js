da78edb76df1b05c2835c5b40416ad2aa8eaabc1
Seongbin Kim <seongbin9786@gmail.com>
2025-04-14 19:29:52 +0900

    chore: install eslint plugins, updated formatter vscode settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,48 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import unusedImports from 'eslint-plugin-unused-imports';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
+export default tseslint.config({
+  extends: [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    prettierRecommended,
+  ],
+  files: ['**/*.{js,jsx,ts,tsx}'],
+  ignores: ['dist', 'routeTree.gen.ts'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
   },
-)
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+    react,
+    'unused-imports': unusedImports,
+  },
+  rules: {
+    'no-unused-vars': 'error',
+    eqeqeq: 'error',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'prefer-template': 'error', // 문자열 연결 대신 템플릿 리터럴 사용
+    'prefer-arrow-callback': 'error', // 일반 함수 대신 화살표 함수 사용
+    'no-plusplus': 'error', // ++ 연산자 사용 금지
+    'no-unneeded-ternary': 'error', // 불필요한 삼항 연산자 사용 금지
+    'unused-imports/no-unused-imports': 'error',
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: 'const', next: 'return' },
+    ],
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+});
