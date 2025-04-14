c0e7fe2becf2c8a58a48172c6358528e914602d1
Seongbin Kim <seongbin9786@gmail.com>
2025-04-14 20:56:57 +0900

    chore: src 기준 절대 경로 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     react(),
     vanillaExtractPlugin(),
+    tsconfigPaths(),
   ],
 });
