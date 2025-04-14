eb37933eb6abd8118dfe9df0bef81424fc21e6fc
Seongbin Kim <seongbin9786@gmail.com>
2025-04-14 20:10:10 +0900

    chore: applied vanilla-extract styles

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,5 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { Link, Outlet, createRootRoute } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
 export const Route = createRootRoute({
   component: Root,
@@ -10,7 +10,7 @@ function Root() {
     <>
       <div>
         <h1>Matchday</h1>
-        <Link to="/">Home</Link> <Link to="/about">About</Link>
+        <Link to='/'>Home</Link> <Link to='/about'>About</Link>
       </div>
       <hr />
       <Outlet />
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,17 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute } from '@tanstack/react-router';
 
-export const Route = createFileRoute("/")({
+import { button } from '../styles/button.css';
+import { lightThemeClass } from '../styles/theme.css';
+
+export const Route = createFileRoute('/')({
   component: HomePage,
 });
 
 function HomePage() {
   return (
-    <div>
+    <div className={lightThemeClass}>
       <h3>Home</h3>
+      <button className={button}>Click me</button>
     </div>
   );
 }
