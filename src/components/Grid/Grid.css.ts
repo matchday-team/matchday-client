547dbaed73178a4dc71d82a1fe75290db14d9f4d
Seongbin Kim <seongbin9786@gmail.com>
2025-04-16 15:02:30 +0900

    feat: 드래그 하이라이트 색상 개선 및 외부 border 제거

--- src/components/Grid/Grid.css.ts
+++ src/components/Grid/Grid.css.ts
@@ -29,39 +29,57 @@ export const gridContainer = style({
   height: '360px', // 하드코딩 없이도 가능한 방법 child 크기로만?
 });
 
-export const gridCell = style({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
-  gap: '4px',
-  transition: 'background-color 0.2s',
-  border: '1px solid rgba(0,0,0,0.2)',
-  cursor: 'pointer',
-
-  width: '60px',
-  height: '60px',
-  ':hover': {
-    backgroundColor: '#226e36',
+const commonGridBorder = style({
+  borderRight: '1px solid rgba(0,0,0,0.15)',
+  borderBottom: '1px solid rgba(0,0,0,0.15)',
+  borderCollapse: 'collapse',
+  selectors: {
+    [`${gridContainer} &:nth-child(5n)`]: {
+      borderRight: 'none',
+    },
+    [`${gridContainer} &:nth-child(n+26)`]: {
+      borderBottom: 'none',
+    },
   },
 });
 
-export const filledCell = style({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
-  gap: '4px',
-  transition: 'background-color 0.2s',
-  border: '1px solid rgba(0,0,0,0.2)',
-  cursor: 'move',
+export const gridCell = style([
+  commonGridBorder,
+  {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: '4px',
+    transition: 'background-color 0.2s',
+    cursor: 'pointer',
 
-  width: '60px',
-  height: '60px',
-  ':hover': {
-    backgroundColor: '#226e36',
+    width: '60px',
+    height: '60px',
+    ':hover': {
+      backgroundColor: '#226e36',
+    },
   },
-});
+]);
+
+export const filledCell = style([
+  commonGridBorder,
+  {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: '4px',
+    transition: 'background-color 0.2s',
+    cursor: 'move',
+
+    width: '60px',
+    height: '60px',
+    ':hover': {
+      backgroundColor: '#226e36',
+    },
+  },
+]);
 
 export const highlightCell = style({
   position: 'relative',
@@ -72,8 +90,8 @@ export const highlightCell = style({
     right: 0,
     bottom: 0,
     left: 0,
-    opacity: 0.2,
-    backgroundColor: '#226e36',
+    opacity: 0.7,
+    backgroundColor: 'black',
     content: '',
   },
 });
