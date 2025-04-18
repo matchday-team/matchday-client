e6f5710f3716aaf3e62a79de5cef5571e76b4472
Seongbin Kim <seongbin9786@gmail.com>
2025-04-18 14:39:19 +0900

    chore: Vite Svgr 플러그인 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
@@ -10,5 +11,6 @@ export default defineConfig({
     react(),
     vanillaExtractPlugin(),
     tsconfigPaths(),
+    svgr(),
   ],
 });
