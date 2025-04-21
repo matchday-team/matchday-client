db752668e8668c39497fcc662e70017e34d4ff33
younaj00 <you.na.joo0@gmail.com>
2025-04-21 20:44:54 +0900

    chore: 절대경로 변경

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,18 +1,18 @@
-export { default as CheckRightIcon } from './check_right.svg';
-export { default as HelpCircleIcon } from './help_circle.svg';
-export { default as HomeBlackIcon } from './home_balck.svg';
-export { default as HomeGreyIcon } from './home_grey.svg';
-export { default as LogoIcon } from './logo.svg';
-export { default as LogoTextIcon } from './logo_text.svg';
-export { default as PlayerBlackIcon } from './player_black.svg';
-export { default as PlayerGreyIcon } from './player_grey.svg';
-export { default as RecordBlackIcon } from './record_black.svg';
-export { default as RecordGreyIcon } from './record_grey.svg';
-export { default as SearchBlackIcon } from './search_black.svg';
-export { default as SearchGreyIcon } from './search_grey.svg';
-export { default as SettingsIcon } from './settings.svg';
-export { default as UserIcon } from './user.svg';
+export { default as CheckRightIcon } from '@/assets/icons/check_right.svg';
+export { default as HelpCircleIcon } from '@/assets/icons/help_circle.svg';
+export { default as HomeBlackIcon } from '@/assets/icons/home_balck.svg';
+export { default as HomeGreyIcon } from '@/assets/icons/home_grey.svg';
+export { default as LogoIcon } from '@/assets/icons/logo.svg';
+export { default as LogoTextIcon } from '@/assets/icons/logo_text.svg';
+export { default as PlayerBlackIcon } from '@/assets/icons/player_black.svg';
+export { default as PlayerGreyIcon } from '@/assets/icons/player_grey.svg';
+export { default as RecordBlackIcon } from '@/assets/icons/record_black.svg';
+export { default as RecordGreyIcon } from '@/assets/icons/record_grey.svg';
+export { default as SearchBlackIcon } from '@/assets/icons/search_black.svg';
+export { default as SearchGreyIcon } from '@/assets/icons/search_grey.svg';
+export { default as SettingsIcon } from '@/assets/icons/settings.svg';
+export { default as UserIcon } from '@/assets/icons/user.svg';
 
 // png
-export { default as SeoulLogo } from './seoul-team.png';
-export { default as SuwonLogo } from './suwon-team.png';
+export { default as SeoulLogo } from '@/assets/icons/seoul-team.png';
+export { default as SuwonLogo } from '@/assets/icons/suwon-team.png';
