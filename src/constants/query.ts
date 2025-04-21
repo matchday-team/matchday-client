8eb9f57eda92e9e9ce4d4b69784f079e91e3984d
Seongbin Kim <seongbin9786@gmail.com>
2025-04-21 09:00:28 +0900

    feat: ky 기반 httpClient, API Response 타입 추가

--- src/constants/query.ts
+++ src/constants/query.ts
@@ -3,3 +3,8 @@ export const QUERY_OPTIONS = {
   GC_TIME: 5_000,
   RETRY_COUNT: 3,
 };
+
+export const FETCH_OPTIONS = {
+  timeout: 3_000,
+  retry: 0,
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,25 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
 
-import { exampleQueryById } from '@/apis';
+import { getTeamsQuery } from '@/apis/teams';
 import { Button, CommonLoader } from '@/components';
 import { queryClient } from '@/react-query-provider';
 import { lightThemeClass } from '@/styles/theme.css';
 
 export const Route = createFileRoute('/')({
   component: HomePage,
-  loader: () => queryClient.ensureQueryData(exampleQueryById(1)),
+  loader: () => queryClient.ensureQueryData(getTeamsQuery),
   pendingComponent: CommonLoader,
 });
 
 function HomePage() {
-  const { data } = useSuspenseQuery(exampleQueryById(1));
+  const { data } = useSuspenseQuery(getTeamsQuery);
 
   return (
     <div className={lightThemeClass}>
       <h3>Home</h3>
       <Button>Click me</Button>
-      <p>{data.message}</p>
+      <p>{JSON.stringify(data)}</p>
     </div>
   );
 }
