ff5080784b132875a11df60bbcad7046ccd9bbee
younaj00 <you.na.joo0@gmail.com>
2025-04-21 20:08:45 +0900

    feat: barrel file

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,18 @@
+export { default as CheckRightIcon } from './check_right.svg';
+export { default as HelpCircleIcon } from './help_circle.svg';
+export { default as HomeBlackIcon } from './home_balck.svg';
+export { default as HomeGreyIcon } from './home_grey.svg';
+export { default as LogoIcon } from './logo.svg';
+export { default as LogoTextIcon } from './logo_text.svg';
+export { default as PlayerBlackIcon } from './player_black.svg';
+export { default as PlayerGreyIcon } from './player_grey.svg';
+export { default as RecordBlackIcon } from './record_black.svg';
+export { default as RecordGreyIcon } from './record_grey.svg';
+export { default as SearchBlackIcon } from './search_black.svg';
+export { default as SearchGreyIcon } from './search_grey.svg';
+export { default as SettingsIcon } from './settings.svg';
+export { default as UserIcon } from './user.svg';
+
+// png
+export { default as SeoulLogo } from './seoul-team.png';
+export { default as SuwonLogo } from './suwon-team.png';
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export * from './CommonLoader';
 export * from './Button';
+export * from './Navbar';
+export * from './Sidebar';
