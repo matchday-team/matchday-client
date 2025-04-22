f584a4d058dc937bf7f844b8afdae61ff7da1ab9
younaj00 <you.na.joo0@gmail.com>
2025-04-22 21:23:42 +0900

    feat: 아이콘 barrel file

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,22 @@
 export { default as PlusIcon } from './plus.svg?react';
 export { default as MinusIcon } from './minus.svg?react';
 export { default as SoccerballIcon } from './soccer-ball.svg?react';
+
+export { default as CheckRightIcon } from '@/assets/icons/check_right.svg';
+export { default as HelpCircleIcon } from '@/assets/icons/help_circle.svg';
+export { default as HomeBlackIcon } from '@/assets/icons/home_balck.svg';
+export { default as HomeGreyIcon } from '@/assets/icons/home_grey.svg';
+export { default as LogoIcon } from '@/assets/icons/logo.svg';
+export { default as LogoTextIcon } from '@/assets/icons/logo_text.svg';
+export { default as PlayerBlackIcon } from '@/assets/icons/player_black.svg';
+export { default as PlayerGreyIcon } from '@/assets/icons/player_grey.svg';
+export { default as RecordBlackIcon } from '@/assets/icons/record_black.svg';
+export { default as RecordGreyIcon } from '@/assets/icons/record_grey.svg';
+export { default as SearchBlackIcon } from '@/assets/icons/search_black.svg';
+export { default as SearchGreyIcon } from '@/assets/icons/search_grey.svg';
+export { default as SettingsIcon } from '@/assets/icons/settings.svg';
+export { default as UserIcon } from '@/assets/icons/user.svg';
+
+// png
+export { default as SeoulLogo } from '@/assets/icons/seoul-team.png';
+export { default as SuwonLogo } from '@/assets/icons/suwon-team.png';
