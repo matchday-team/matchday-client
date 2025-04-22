9bc7fc81276987cf49b5360b8eb9b8d34aece99c
younaj00 <you.na.joo0@gmail.com>
2025-04-22 21:32:48 +0900

    feat: 루트 레이아웃 구성

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,53 @@
+import { useEffect, useState } from 'react';
+
 import { Outlet, createRootRoute } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+
+import { Navbar } from '@/components/Navbar';
+import { Sidebar } from '@/components/Sidebar';
+import { SidebarProvider } from '@/components/Sidebar/context';
+import { lightThemeVars } from '@/styles/theme.css';
 
 export const Route = createRootRoute({
   component: Root,
 });
 
+function MainLayout() {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 1440);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <Sidebar />
+      <main
+        style={{
+          paddingTop: '64px',
+          paddingLeft: isSmallScreen ? '60px' : '360px',
+          backgroundColor: lightThemeVars.color.white.hover,
+          minHeight: '100vh',
+          width: isSmallScreen ? 'calc(100% - 62px)' : 'calc(100% - 360px)',
+          marginLeft: 'auto',
+        }}
+      >
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
 function Root() {
   return (
-    <>
-      <Outlet />
-      <TanStackRouterDevtools />
-    </>
+    <SidebarProvider>
+      <MainLayout />
+    </SidebarProvider>
   );
 }
