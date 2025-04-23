5a595dbee979d9cc5bdc2335de2ffa93a32a5f59
younaj00 <you.na.joo0@gmail.com>
2025-04-23 11:33:51 +0900

    fix: 페이지 빌드 에러

--- src/routes/help.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-
-export const Route = createFileRoute('/help')({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <div>도움말 페이지 입니다!</div>;
-}
