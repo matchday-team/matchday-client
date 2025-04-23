2df6642dab8185b5b258f418c534f1a55e5d9bae
younaj00 <you.na.joo0@gmail.com>
2025-04-23 11:33:51 +0900

    fix: 페이지 빌드 에러

--- src/routes/settings.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-
-export const Route = createFileRoute('/settings')({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <div>설정 페이지 입니다!</div>;
-}
