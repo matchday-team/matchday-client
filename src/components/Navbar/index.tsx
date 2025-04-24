a413f59804520106000e549f7d67eb43c602b8f3
younaj00 <you.na.joo0@gmail.com>
2025-04-25 01:02:30 +0900

    refactor: 중복 렌더링 코드 단순화

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,20 +1,12 @@
-import { useMatches } from '@tanstack/react-router';
-
 import * as styles from '@/components/Navbar/navbar.css';
 import { useSidebar } from '@/components/Sidebar/context';
 
 export function Navbar() {
-  const matches = useMatches();
-  const currentRoute = matches[matches.length - 1];
   const { width } = useSidebar();
 
   return (
     <header className={styles.navbar} style={{ left: `${width}px` }}>
-      <h1 className={styles.title}>
-        {currentRoute?.pathname === '/'
-          ? '교내 리그 3R 매치 기록'
-          : '교내 리그 3R 매치 기록'}
-      </h1>
+      <h1 className={styles.title}>{'교내 리그 3R 매치 기록'}</h1>
       <div className={styles.actions}>
         <button className={styles.primaryButton}>등록하기</button>
       </div>
