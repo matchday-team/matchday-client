7288d715a85b6f992091e698d676499de65d7c2d
younaj00 <you.na.joo0@gmail.com>
2025-04-25 16:16:56 +0900

    refactor: 형제/자식 모듈 import를 상대 경로로 수정

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,13 @@
-import * as styles from '@/components/Navbar/navbar.css';
 import { useSidebar } from '@/components/Sidebar/context';
+
+import * as styles from './navbar.css';
 
 export function Navbar() {
   const { width } = useSidebar();
 
   return (
     <header className={styles.navbar} style={{ left: `${width}px` }}>
-      <h1 className={styles.title}>{'교내 리그 3R 매치 기록'}</h1>
+      <h1 className={styles.title}>교내 리그 3R 매치 기록</h1>
       <div className={styles.actions}>
         <button className={styles.primaryButton}>등록하기</button>
       </div>
