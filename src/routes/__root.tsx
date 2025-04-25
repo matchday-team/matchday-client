8e0a00dab5af7ddfc990c8e71e5678fa540cbeb7
younaj00 <you.na.joo0@gmail.com>
2025-04-25 10:15:46 +0900

    fix: 상수들을 UPPER_CASE로 변경

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,7 +3,20 @@ import { useEffect, useState } from 'react';
 import { Outlet, createRootRoute } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
+<<<<<<< HEAD
 import { MainLayout, SidebarProvider } from '@/components';
+=======
+import { Navbar } from '@/components/Navbar';
+import { Sidebar } from '@/components/Sidebar';
+import { SidebarProvider } from '@/components/Sidebar/context';
+import {
+  NAVBAR_HEIGHT,
+  SIDEBAR_BREAKPOINT,
+  SIDEBAR_WIDTH,
+  SIDEBAR_WIDTH_TOGGLE,
+} from '@/constants';
+import { lightThemeVars } from '@/styles/theme.css';
+>>>>>>> 0af99f3 (fix: 상수들을 UPPER_CASE로 변경)
 
 export const Route = createRootRoute({
   component: Root,
@@ -14,7 +27,7 @@ function MainLayout() {
 
   useEffect(() => {
     const handleResize = () => {
-      setIsSmallScreen(window.innerWidth <= sidebarBreakpoint);
+      setIsSmallScreen(window.innerWidth <= SIDEBAR_BREAKPOINT);
     };
 
     handleResize();
@@ -22,7 +35,9 @@ function MainLayout() {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  const currentSidebarWidth = isSmallScreen ? sidebarWidthToggle : sidebarWidth;
+  const currentSidebarWidth = isSmallScreen
+    ? SIDEBAR_WIDTH_TOGGLE
+    : SIDEBAR_WIDTH;
 
   return (
     <div>
@@ -30,7 +45,7 @@ function MainLayout() {
       <Sidebar />
       <main
         style={{
-          paddingTop: navbarHeight,
+          paddingTop: NAVBAR_HEIGHT,
           paddingLeft: currentSidebarWidth,
           backgroundColor: lightThemeVars.color.white.hover,
           minHeight: '100vh',
