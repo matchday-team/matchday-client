0114d583158a7080b85ca2c3316316c60618ec87
Seongbin Kim <seongbin9786@gmail.com>
2025-04-25 12:56:38 +0900

    fix: Storybook ESLint 규칙이 stories 파일 외에도 적용되는 현상 수정

--- eslint.config.js
+++ eslint.config.js
@@ -10,74 +10,77 @@ import unusedImports from 'eslint-plugin-unused-imports';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config({
-  extends: [
-    js.configs.recommended,
-    ...tseslint.configs.recommended,
-    prettierRecommended,
-    react.configs.flat.recommended,
-    react.configs.flat['jsx-runtime'], // @see https://github.com/jsx-eslint/eslint-plugin-react
-    reactHooks.configs['recommended-latest'], // @see https://github.com/facebook/react/tree/main/packages/eslint-plugin-react-hooks
-    reactRefresh.configs.vite, // @see https://github.com/ArnaudBarre/eslint-plugin-react-refresh?tab=readme-ov-file#recommended-config
-    ...tanStackQuery.configs['flat/recommended'], // @see https://tanstack.com/query/v5/docs/eslint/eslint-plugin-query#recommended-setup
-    {
-      name: vanillaExtract.meta.name,
-      plugins: {
-        'vanilla-extract': vanillaExtract,
+export default tseslint.config(
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettierRecommended,
+      react.configs.flat.recommended,
+      react.configs.flat['jsx-runtime'], // @see https://github.com/jsx-eslint/eslint-plugin-react
+      reactHooks.configs['recommended-latest'], // @see https://github.com/facebook/react/tree/main/packages/eslint-plugin-react-hooks
+      reactRefresh.configs.vite, // @see https://github.com/ArnaudBarre/eslint-plugin-react-refresh?tab=readme-ov-file#recommended-config
+      ...tanStackQuery.configs['flat/recommended'], // @see https://tanstack.com/query/v5/docs/eslint/eslint-plugin-query#recommended-setup
+      {
+        name: vanillaExtract.meta.name,
+        plugins: {
+          'vanilla-extract': vanillaExtract,
+        },
+        rules: vanillaExtract.configs.recommended.rules,
       },
-      rules: vanillaExtract.configs.recommended.rules,
+    ],
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['dist', 'routeTree.gen.ts'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
-    storybook.configs['flat/recommended'],
-  ],
-  files: ['**/*.{js,jsx,ts,tsx}'],
-  ignores: ['dist', 'routeTree.gen.ts'],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', prev: 'const', next: 'return' },
+      ],
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: [
+            {
+              group: ['../*'],
+              message: '상위 디렉토리에 대한 상대 경로 대신 절대 경로 사용',
+            },
+          ],
+        },
+      ],
+      eqeqeq: 'error',
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'object-shorthand': 'error',
+      'prefer-template': 'error', // 문자열 연결 대신 템플릿 리터럴 사용
+      'prefer-arrow-callback': 'error', // 일반 함수 대신 화살표 함수 사용
+      'no-plusplus': 'error', // ++ 연산자 사용 금지
+      'no-unneeded-ternary': 'error', // 불필요한 삼항 연산자 사용 금지
+    },
   },
-  plugins: {
-    'unused-imports': unusedImports,
+  {
+    files: ['**/*.stories.{ts,tsx}'],
+    extends: [storybook.configs['flat/recommended']],
   },
-  rules: {
-    '@typescript-eslint/no-unused-vars': 'off',
-    'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': [
-      'warn',
-      {
-        vars: 'all',
-        varsIgnorePattern: '^_',
-        args: 'after-used',
-        argsIgnorePattern: '^_',
-      },
-    ],
-    'storybook/story-exports': 'off',
-    'storybook/default-exports': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'padding-line-between-statements': [
-      'error',
-      { blankLine: 'always', prev: 'const', next: 'return' },
-    ],
-    'no-restricted-imports': [
-      'error',
-      {
-        patterns: [
-          {
-            group: ['../*'],
-            message: '상위 디렉토리에 대한 상대 경로 대신 절대 경로 사용',
-          },
-        ],
-      },
-    ],
-    eqeqeq: 'error',
-    'prefer-const': 'error',
-    'no-var': 'error',
-    'object-shorthand': 'error',
-    'prefer-template': 'error', // 문자열 연결 대신 템플릿 리터럴 사용
-    'prefer-arrow-callback': 'error', // 일반 함수 대신 화살표 함수 사용
-    'no-plusplus': 'error', // ++ 연산자 사용 금지
-    'no-unneeded-ternary': 'error', // 불필요한 삼항 연산자 사용 금지
-  },
-});
+);
