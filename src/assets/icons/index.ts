e805a17d87ee2db8a66b63680fdb11c72710430f
younaj00 <you.na.joo0@gmail.com>
2025-04-26 19:52:57 +0900

    chore: SVG 아이콘 SVGR 플러그인 적용 및 export 정리

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,22 +1,16 @@
 export { default as PlusIcon } from './plus.svg?react';
 export { default as MinusIcon } from './minus.svg?react';
-export { default as SoccerballIcon } from './soccer-ball.svg?react';
-
-export { default as CheckRightIcon } from '@/assets/icons/check_right.svg';
-export { default as HelpCircleIcon } from '@/assets/icons/help_circle.svg';
-export { default as HomeBlackIcon } from '@/assets/icons/home_black.svg';
-export { default as HomeGreyIcon } from '@/assets/icons/home_grey.svg';
-export { default as LogoIcon } from '@/assets/icons/logo.svg';
-export { default as LogoTextIcon } from '@/assets/icons/logo_text.svg';
-export { default as PlayerBlackIcon } from '@/assets/icons/player_black.svg';
-export { default as PlayerGreyIcon } from '@/assets/icons/player_grey.svg';
-export { default as RecordBlackIcon } from '@/assets/icons/record_black.svg';
-export { default as RecordGreyIcon } from '@/assets/icons/record_grey.svg';
-export { default as SearchBlackIcon } from '@/assets/icons/search_black.svg';
-export { default as SearchGreyIcon } from '@/assets/icons/search_grey.svg';
-export { default as SettingsIcon } from '@/assets/icons/settings.svg';
-export { default as UserIcon } from '@/assets/icons/user.svg';
+export { default as CheckRightIcon } from './check_right.svg?react';
+export { default as HelpCircleIcon } from './help_circle.svg?react';
+export { default as HomeIcon } from './home.svg?react';
+export { default as LogoIcon } from './logo.svg?react';
+export { default as LogoTextIcon } from './logo_text.svg?react';
+export { default as PlayerIcon } from './player.svg?react';
+export { default as RecordIcon } from './record.svg?react';
+export { default as SearchIcon } from './search.svg?react';
+export { default as SettingsIcon } from './settings.svg?react';
+export { default as UserIcon } from './user.svg?react';
 
 // png
-export { default as SeoulLogo } from '@/assets/icons/seoul-team.png';
-export { default as SuwonLogo } from '@/assets/icons/suwon-team.png';
+export { default as SeoulLogo } from './seoul-team.png';
+export { default as SuwonLogo } from './suwon-team.png';
