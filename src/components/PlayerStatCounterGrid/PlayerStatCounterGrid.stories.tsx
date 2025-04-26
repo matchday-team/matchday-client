ea76a2f9affb5e883af42d080ab5bd78796974d9
Seongbin Kim <seongbin9786@gmail.com>
2025-04-26 13:00:06 +0900

    docs: MAT-80 선수 스텟 카운터 컴포넌트 문서화

---
+++ src/components/PlayerStatCounterGrid/PlayerStatCounterGrid.stories.tsx
@@ -0,0 +1,56 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { assignInlineVars } from '@vanilla-extract/dynamic';
+
+import { teamColor } from '@/components/PlayerList/TeamColor.css';
+import { lightThemeVars } from '@/styles/theme.css';
+
+import { PlayerStatCounterGrid } from './PlayerStatCounterGrid';
+
+const meta = {
+  title: 'Components/PlayerStatCounterGrid',
+  component: PlayerStatCounterGrid,
+  parameters: {
+    layout: 'centered',
+  },
+  decorators: [
+    Story => (
+      <div
+        style={{
+          width: '280px',
+          ...assignInlineVars({
+            [teamColor]: lightThemeVars.color.soccer.red,
+          }),
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+  tags: ['autodocs'],
+} satisfies Meta<typeof PlayerStatCounterGrid>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    team: {
+      logoImageUrl: 'https://via.placeholder.com/150',
+      id: 1,
+      name: '홍길동',
+      teamColor: '#000000',
+    },
+    player: {
+      number: 91,
+      name: '홍길동',
+      position: 'FW',
+      profileImageUrl: 'https://via.placeholder.com/150',
+      goals: 10,
+      assists: 5,
+      fouls: 3,
+      id: 1,
+      yellowCards: 1,
+      redCards: 0,
+    },
+  },
+};
