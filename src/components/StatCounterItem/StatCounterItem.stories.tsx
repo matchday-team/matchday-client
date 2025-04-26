f8e9e2bc68d7456f7bf008094b81e54ab06fb30c
Seongbin Kim <seongbin9786@gmail.com>
2025-04-26 11:43:04 +0900

    docs: MAT-80 StatCounterItem 컴포넌트 문서화

---
+++ src/components/StatCounterItem/StatCounterItem.stories.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+
+import type { Meta, StoryObj } from '@storybook/react';
+import { assignInlineVars } from '@vanilla-extract/dynamic';
+
+import { teamColor } from '@/components/PlayerList/TeamColor.css';
+import { lightThemeVars } from '@/styles/theme.css';
+
+import { StatCounterItem } from './StatCounterItem';
+
+const meta = {
+  title: 'components/StatCounterItem',
+  component: StatCounterItem,
+  parameters: {
+    layout: 'centered',
+  },
+
+  decorators: [
+    Story => (
+      <div
+        style={assignInlineVars({
+          [teamColor]: lightThemeVars.color.soccer.red,
+        })}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+  tags: ['autodocs'],
+} satisfies Meta<typeof StatCounterItem>;
+
+export default meta;
+type Story = StoryObj<typeof StatCounterItem>;
+
+export const Default: Story = {
+  args: {
+    title: '득점',
+    value: 0,
+  },
+};
+
+export const WithParentState: Story = {
+  args: {
+    title: '득점',
+    value: 2,
+  },
+  render: args => {
+    const [value, setValue] = useState(args.value);
+
+    return (
+      <div
+        style={assignInlineVars({
+          [teamColor]: lightThemeVars.color.soccer.red,
+        })}
+      >
+        <StatCounterItem
+          {...args}
+          value={value}
+          onIncrement={() => setValue(value + 1)}
+          onDecrement={() => setValue(value - 1)}
+        />
+      </div>
+    );
+  },
+};
