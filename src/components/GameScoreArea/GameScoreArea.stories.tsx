95fb80c0b796435c425b270ef4972a9e3598eaa0
Seongbin Kim <seongbin9786@gmail.com>
2025-04-27 21:04:56 +0900

    docs: MAT-116 게임 스코어 영역 컴포넌트 Storybook 문서화

---
+++ src/components/GameScoreArea/GameScoreArea.stories.tsx
@@ -0,0 +1,72 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { assignInlineVars } from '@vanilla-extract/dynamic';
+
+import FCSeoulLogo from '@/assets/images/teams/fcseoul.png';
+import FCSwonLogo from '@/assets/images/teams/fcsuwon.png';
+import {
+  teamAwayColor,
+  teamHomeColor,
+} from '@/components/MatchLogList/colors.css';
+
+import { GameScoreArea } from './GameScoreArea';
+
+const meta = {
+  title: 'Components/GameScoreArea',
+  component: GameScoreArea,
+  parameters: {
+    layout: 'centered',
+  },
+  decorators: [
+    Story => (
+      <div
+        style={{
+          width: 300,
+          ...assignInlineVars({
+            [teamHomeColor]: '#D91920',
+            [teamAwayColor]: '#003A70',
+          }),
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+  tags: ['autodocs'],
+} satisfies Meta<typeof GameScoreArea>;
+
+export default meta;
+type Story = StoryObj<typeof GameScoreArea>;
+
+export const Default: Story = {
+  args: {
+    scores: {
+      home: 0,
+      away: 0,
+    },
+    homeTeam: {
+      name: 'FC 서울',
+      logoImageUrl: FCSeoulLogo,
+    },
+    awayTeam: {
+      name: 'FC 수원',
+      logoImageUrl: FCSwonLogo,
+    },
+  },
+};
+
+export const Custom: Story = {
+  args: {
+    scores: {
+      home: 3,
+      away: 1,
+    },
+    homeTeam: {
+      name: '한양대',
+      logoImageUrl: FCSeoulLogo,
+    },
+    awayTeam: {
+      name: '동국대',
+      logoImageUrl: FCSwonLogo,
+    },
+  },
+};
