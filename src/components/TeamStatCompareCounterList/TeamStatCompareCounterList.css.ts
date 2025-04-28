6d39ba6b94222ea4d6bcfd0a8c986ba35959d6d3
Seongbin Kim <seongbin9786@gmail.com>
2025-04-28 15:19:09 +0900

    fix: MAT-117 padding shorthand 적용 (코드래빗 리뷰 반영)

--- src/components/TeamStatCompareCounterList/TeamStatCompareCounterList.css.ts
+++ src/components/TeamStatCompareCounterList/TeamStatCompareCounterList.css.ts
@@ -4,8 +4,5 @@ export const container = style({
   display: 'flex',
   flexDirection: 'column',
   gap: 24,
-  paddingTop: 22,
-  paddingRight: 20,
-  paddingBottom: 22,
-  paddingLeft: 20,
+  padding: [22, 20],
 });
