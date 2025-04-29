827fa3bfa7cfa10fee834146877f55c5d156c371
younaj00 <you.na.joo0@gmail.com>
2025-04-30 04:23:39 +0900

    fix: 타입스크립트 경로 타입 오류 수정

--- src/routes/search.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-
-export const Route = createFileRoute('/search')({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <div>매치 조회 입니다!</div>;
-}
