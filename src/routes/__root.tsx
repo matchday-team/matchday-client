1dc20b601b41ba7d9c975bf29124a944d5381612
younaj00 <you.na.joo0@gmail.com>
2025-04-30 21:23:41 +0900

    refactor: MainLayout 컴포넌트를 __root.tsx에서 분리

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,59 +1,22 @@
-import { useEffect, useState } from 'react';
-
 import { Outlet, createRootRoute } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
-import { Layout, SidebarProvider } from '@/components';
-import {
-  NAVBAR_HEIGHT,
-  SIDEBAR_BREAKPOINT,
-  SIDEBAR_WIDTH,
-  SIDEBAR_WIDTH_TOGGLE,
-} from '@/constants';
-import { lightThemeVars } from '@/styles/theme.css';
->>>>>>> 0af99f3 (fix: 상수들을 UPPER_CASE로 변경)
+import { MainLayout, SidebarProvider } from '@/components';
 
 export const Route = createRootRoute({
   component: Root,
 });
 
-function MainLayout() {
-  const [isSmallScreen, setIsSmallScreen] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsSmallScreen(window.innerWidth <= SIDEBAR_BREAKPOINT);
-    };
-
-    handleResize();
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  const currentSidebarWidth = isSmallScreen
-    ? SIDEBAR_WIDTH_TOGGLE
-    : SIDEBAR_WIDTH;
-
-  return (
-    <Layout>
-      <main
-        style={{
-          paddingTop: NAVBAR_HEIGHT,
-          paddingLeft: currentSidebarWidth,
-          backgroundColor: lightThemeVars.color.white.hover,
-          minHeight: '100vh',
-          width: `calc(100% - ${currentSidebarWidth}px)`,
-          marginLeft: 'auto',
-        }}
-      >
-        <Outlet />
-      </main>
-    </Layout>
-  );
-}
-
 function Root() {
   return (
+    <>
+      <SidebarProvider>
+        <MainLayout />
+      </SidebarProvider>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
     <>
       <SidebarProvider>
         <MainLayout />
