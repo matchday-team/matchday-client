95329ffd40ced848be9ffd1fe929207793f14c70
Seongbin Kim <seongbin9786@gmail.com>
2025-05-01 12:01:57 +0900

    feat: MAT-81 선발 선수 그리드 - 크기를 외부에서 설정하게 변경

--- src/components/PlayerOnFieldGrid/FieldBackground/FieldBackground.css.ts
+++ src/components/PlayerOnFieldGrid/FieldBackground/FieldBackground.css.ts
@@ -6,6 +6,7 @@ export const rootContainer = style({
   position: 'relative',
   borderRadius: 10,
   backgroundColor: lightThemeVars.color.field.background,
+  width: '100%',
   height: '100%',
   overflow: 'hidden', // NOTE: 각 Cell에서 bgColor를 지정하므로 border 부분을 잘리게 표현하기 위함
 });
