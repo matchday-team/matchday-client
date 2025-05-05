9630121d3259f2e28040b0c3ba7e63c5e58c29ff
younaj00 <you.na.joo0@gmail.com>
2025-05-05 18:24:51 +0900

    feat: SVG 아이콘 SVGR export 적용

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,5 +1,17 @@
 export { default as PlusIcon } from './plus.svg?react';
 export { default as MinusIcon } from './minus.svg?react';
+export { default as CheckRightIcon } from './check_right.svg?react';
+export { default as HelpCircleIcon } from './help_circle.svg?react';
+export { default as HomeIcon } from './home.svg?react';
+export { default as LogoIcon } from './logo.svg?react';
+export { default as LogoTextIcon } from './logo_text.svg?react';
+export { default as PlayerIcon } from './player.svg?react';
+export { default as RecordIcon } from './record.svg?react';
+export { default as SearchIcon } from './search.svg?react';
+export { default as SettingsIcon } from './settings.svg?react';
+export { default as UserIcon } from './user.svg?react';
 export { default as SoccerballIcon } from './soccer-ball.svg?react';
+export { default as SeoulLogo } from './seoul-team.png';
+export { default as SuwonLogo } from './suwon-team.png';
 export { default as ExclamationIcon } from './exclamation.svg?react';
 export { default as ChevronDownIcon } from './chevron-down.svg?react';
