14f32b57f15efbf1a8c69de1bb344fca8757c3c5
younaj00 <you.na.joo0@gmail.com>
2025-05-05 20:48:56 +0900

    chore: 사용되지 않는 아이콘 SVG 제거 및 통일성을 위해 파일명 수정

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,10 +1,10 @@
 export { default as PlusIcon } from './plus.svg?react';
 export { default as MinusIcon } from './minus.svg?react';
-export { default as CheckRightIcon } from './check_right.svg?react';
-export { default as HelpCircleIcon } from './help_circle.svg?react';
+export { default as CheckRightIcon } from './check-right.svg?react';
+export { default as HelpCircleIcon } from './help-circle.svg?react';
 export { default as HomeIcon } from './home.svg?react';
 export { default as LogoIcon } from './logo.svg?react';
-export { default as LogoTextIcon } from './logo_text.svg?react';
+export { default as LogoTextIcon } from './logo-text.svg?react';
 export { default as PlayerIcon } from './player.svg?react';
 export { default as RecordIcon } from './record.svg?react';
 export { default as SearchIcon } from './search.svg?react';
