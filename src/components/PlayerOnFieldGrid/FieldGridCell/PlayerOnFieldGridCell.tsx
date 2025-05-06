f8838e766fe9db34bb0c0bb93fda9efe58b27b0c
Seongbin Kim <seongbin9786@gmail.com>
2025-05-06 19:52:34 +0900

    docs: MAT-153 당장 할 수 없는 작업 리스트업

--- src/components/PlayerOnFieldGrid/FieldGridCell/PlayerOnFieldGridCell.tsx
+++ src/components/PlayerOnFieldGrid/FieldGridCell/PlayerOnFieldGridCell.tsx
@@ -26,12 +26,23 @@ export const PlayerOnFieldGridCell = ({
     e.dataTransfer.dropEffect = 'move'; // Q. 필요할까?
   };
 
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    console.log('drop', e, e.dataTransfer.getData('application/json'));
+
+    // NOTE:
+    // 1. ws API 호출 (Promise 반환)
+    // 2. Promise 대기
+    // 3. 성공 시 setQueryData로 업데이트
+    // 4. 100% 서버 상태에 의존하기 때문에 현재 구현할 수 없는 기능이긴 함
+  };
+
   return (
     <div
       className={commonCellContainer({ isSelected, isDragOver })}
       onClick={onClick}
       ref={hoverTargetRef}
       onDragOver={handleDragOver}
+      onDrop={handleDrop}
     >
       <div className={styles.playerImageContainer}>
         <img
