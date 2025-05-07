cd4a0a6d638ea08936ba3ac6b7075c321715dca0
younaj00 <you.na.joo0@gmail.com>
2025-05-07 23:25:55 +0900

    refactor: 중복된 SidebarProvider 제거

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,7 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
-import { MainLayout, SidebarProvider } from '@/components';
+import { MainLayout } from '@/components';
 
 export const Route = createRootRoute({
   component: Root,
@@ -10,9 +10,7 @@ export const Route = createRootRoute({
 function Root() {
   return (
     <>
-      <SidebarProvider>
-        <MainLayout />
-      </SidebarProvider>
+      <MainLayout />
       <Outlet />
       <TanStackRouterDevtools />
     </>
