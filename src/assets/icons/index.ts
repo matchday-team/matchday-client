33c501576974f3c3a9363d6445e51ca062779ae7
Seongbin Kim <seongbin9786@gmail.com>
2025-05-09 10:09:25 +0900

    chore: MAT-175 느낌표 아이콘 추가

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as PlusIcon } from './plus.svg?react';
 export { default as MinusIcon } from './minus.svg?react';
 export { default as SoccerballIcon } from './soccer-ball.svg?react';
+export { default as ExclamationIcon } from './exclamation.svg?react';
