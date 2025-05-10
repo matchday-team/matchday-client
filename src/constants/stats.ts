c3d892dbe4ef6f4a63d944611a63cbb947683531
Seongbin Kim <seongbin9786@gmail.com>
2025-05-10 11:52:00 +0900

    feat: MAT-182 팀 스텟 가짓수를 8종으로 변경

--- src/constants/stats.ts
+++ src/constants/stats.ts
@@ -1,21 +1,33 @@
 import { ScoreResponse } from '@/apis/models';
 
-type Stat = '슈팅' | '유효 슈팅' | '코너 킥' | '오프 사이드' | '파울' | '경고';
+type Stat =
+  | '득점'
+  | '슈팅'
+  | '유효 슈팅'
+  | '코너 킥'
+  | '오프 사이드'
+  | '파울'
+  | '경고'
+  | '자책골';
 
 export const STAT_LIST: Stat[] = [
+  '득점',
   '슈팅',
   '유효 슈팅',
   '코너 킥',
   '오프 사이드',
   '파울',
   '경고',
+  '자책골',
 ];
 
 export const statMapper: Record<Stat, keyof ScoreResponse> = {
+  득점: 'goalCount',
   슈팅: 'goalCount',
   '유효 슈팅': 'validShotCount',
   '코너 킥': 'cornerKickCount',
   '오프 사이드': 'offsideCount',
   파울: 'foulCount',
   경고: 'warningCount',
+  자책골: 'ownGoalCount',
 };
