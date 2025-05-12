b3acb75dcc6e977fddb24ee0450d3f35e0ea052b
younaj00 <you.na.joo0@gmail.com>
2025-05-12 20:08:39 +0900

    refactor: NavItem과 NavItem.css 서브 컴포넌트로 분리

---
+++ src/components/Sidebar/FooterItem/FooterItem.css.ts
@@ -0,0 +1,43 @@
+import { style } from '@vanilla-extract/css';
+
+import {
+  footer,
+  footerItemIcon,
+  label,
+  transitionTiming,
+} from '@/components/Sidebar/sidebarCommon.css.ts';
+import { lightThemeVars } from '@/styles/theme.css';
+
+export { footer, footerItemIcon, label, transitionTiming };
+
+export const footerItem = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  transition: `background-color 0.3s ${transitionTiming}`,
+  width: '100%',
+  height: 44,
+  overflow: 'hidden',
+  textDecoration: 'none',
+  whiteSpace: 'nowrap',
+  color: lightThemeVars.color.gray[500],
+  fontSize: 14,
+  selectors: {
+    '&:hover': {
+      backgroundColor: lightThemeVars.color.white.hover,
+    },
+    '&:focus-visible': {
+      outline: 'none',
+      boxShadow: `0 0 0 2px ${lightThemeVars.color.primary[300]}`,
+      backgroundColor: lightThemeVars.color.white.hover,
+    },
+  },
+});
+
+export const footerRightIcon = style({
+  position: 'relative',
+  right: 34,
+  flexShrink: 0,
+  width: 24,
+  height: 24,
+});
