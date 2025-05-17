f6163c1cbd66c6a130e18bb14c0d986e9eda33b3
Seongbin Kim <seongbin9786@gmail.com>
2025-05-17 11:26:02 +0900

    fix: MAT-181 교체가 비활성화된 선수가 드래그되지 않을 때 텍스트 드래그가 되어 혼란스러운 점 수정

--- src/components/PlayerOnFieldGrid/FieldGridCell/commonStyle.css.ts
+++ src/components/PlayerOnFieldGrid/FieldGridCell/commonStyle.css.ts
@@ -10,9 +10,10 @@ export const commonCellContainer = recipe({
     alignItems: 'center',
     justifyContent: 'center',
     border: '2px solid transparent',
-    // NOTE: border width 변동 시 이상한 Layout Shift가 발생함 - 항상 동일 width를 사용하고, 대신 boxShadow로 기본 border를 대체함
     boxShadow: `0 0 0 1px ${lightThemeVars.color.field.lineLight}`,
-    height: '100%', // FIXME: 겹치는 모서리 부분에는 더 색이 진한 문제가 존재함
+    // NOTE: border width 변동 시 이상한 Layout Shift가 발생함 - 항상 동일 width를 사용하고, 대신 boxShadow로 기본 border를 대체함
+    height: '100%',
+    userSelect: 'none', // FIXME: 겹치는 모서리 부분에는 더 색이 진한 문제가 존재함
     color: lightThemeVars.color.white.main,
 
     ':hover': {
