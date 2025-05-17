8fdb929d1fc0d152493dbeb5b8b8be6c208c001f
Seongbin Kim <seongbin9786@gmail.com>
2025-05-17 11:41:58 +0900

    feat: MAT-181 선발/교체 명단을 정책대로 정렬

--- src/routes/matches/$matchId/record/route.tsx
+++ src/routes/matches/$matchId/record/route.tsx
@@ -66,6 +66,22 @@ const dividePlayers = ({ starters, substitutes }: TeamGroupedUsers) => {
     targetArray.push(player);
   });
 
+  result.starters.sort((a, b) => (b.matchGrid ?? 0) - (a.matchGrid ?? 0));
+
+  result.substitutes.sort((a, b) => {
+    const aDisabled = a.subOut || a.sentOff;
+    const bDisabled = b.subOut || b.sentOff;
+
+    // 둘 다 disabled거나 아닐 때는 matchGrid 순으로 정렬
+    if (aDisabled === bDisabled) {
+      return (b.matchGrid ?? 0) - (a.matchGrid ?? 0);
+    } else if (aDisabled && !bDisabled) {
+      return 1; // a만 disabled면 a를 뒤로
+    } else {
+      return -1; // b만 disabled면 b를 뒤로
+    }
+  });
+
   return result;
 };
 
