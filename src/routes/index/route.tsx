f5547df36d73292a920e84307caa5534866de5a8
Seongbin Kim <seongbin9786@gmail.com>
2025-05-18 21:33:41 +0900

    feat: MAT-233 임시 매치 리스트 컴포넌트 추가 (for test)

---
+++ src/routes/index/route.tsx
@@ -0,0 +1,42 @@
+import { useQuery, useSuspenseQuery } from '@tanstack/react-query';
+import { createFileRoute } from '@tanstack/react-router';
+
+import { matchRecordQuery, userQuery } from '@/apis/queries';
+import { CommonLoader } from '@/components';
+import { queryClient } from '@/react-query-provider';
+
+import { TeamList } from './-components';
+import * as styles from './-components/MyTeamMatchListPage.css';
+
+export const Route = createFileRoute('/')({
+  component: MyTeamMatchListPage,
+  loader: () => {
+    return queryClient.ensureQueryData(userQuery.meQuery);
+  },
+});
+
+function MyTeamMatchListPage() {
+  const { data: user } = useSuspenseQuery(userQuery.meQuery);
+  const { data: matchList, isLoading } = useQuery({
+    ...matchRecordQuery.listQuery(user!.teamId!),
+    enabled: Boolean(user?.teamId),
+  });
+
+  // NOTE: enabled 때문에 타입 가드가 안 됨
+  if (isLoading || !matchList) {
+    return <CommonLoader />;
+  }
+
+  console.log('matches:', matchList);
+
+  return (
+    <div className={styles.rootContainer}>
+      <div className={styles.header}>
+        <div className={styles.headerTitle}>FC서울</div>
+      </div>
+      <div className={styles.listContainer}>
+        <TeamList matchList={matchList.data} />
+      </div>
+    </div>
+  );
+}
