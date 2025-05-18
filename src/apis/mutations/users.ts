c6093a1d7c7648fb7c9a555391d0c8785a0be450
Seongbin Kim <seongbin9786@gmail.com>
2025-05-18 20:07:16 +0900

    feat: MAT-216 더미 로그인 Mutation 추가

--- src/apis/mutations/users.ts
+++ src/apis/mutations/users.ts
@@ -1,8 +1,13 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useNavigate } from '@tanstack/react-router';
 import { HTTPError } from 'ky';
 import { useSnackbar } from 'notistack';
 
 import { userApi } from '@/apis/fetchers';
+import { userQuery } from '@/apis/queries';
+import { tempUser } from '@/constants/tempUser';
+import { useLoggedInUserStore } from '@/stores';
+import { delay } from '@/utils';
 
 export const useCreateUserMutation = () => {
   const { enqueueSnackbar } = useSnackbar();
@@ -25,6 +30,39 @@ export const useCreateUserMutation = () => {
           },
         );
       }
+    },
+  });
+};
+
+export const useLoginMutation = () => {
+  const { enqueueSnackbar } = useSnackbar();
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const { setIsLoggedIn } = useLoggedInUserStore();
+
+  return useMutation({
+    mutationFn: async ({ id, pw }: { id: string; pw: string }) => {
+      return delay(500).then(() => {
+        if (id === 'matchday' && pw === 'matchday') {
+          return tempUser;
+        }
+
+        return Promise.reject(new Error('로그인 실패'));
+      });
+    },
+    onSuccess: () => {
+      enqueueSnackbar('로그인 성공', {
+        variant: 'success',
+      });
+
+      setIsLoggedIn(true);
+      queryClient.invalidateQueries({ queryKey: userQuery.queryKeys.me() });
+      navigate({ to: '/' });
+    },
+    onError: () => {
+      enqueueSnackbar('로그인 실패', {
+        variant: 'error',
+      });
     },
   });
 };
