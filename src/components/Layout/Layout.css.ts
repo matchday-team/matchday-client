628d9dddb8dcb77f73238a64f6d97122bcec9d11
Seongbin Kim <seongbin9786@gmail.com>
2025-05-21 20:12:02 +0900

    fix: MAT-267 사이드바 오동작 제거 (breakpoint 개선, 여백 개선)

--- src/components/Layout/Layout.css.ts
+++ src/components/Layout/Layout.css.ts
@@ -1,6 +1,10 @@
 import { style } from '@vanilla-extract/css';
 
-import { NAVBAR_HEIGHT, SIDEBAR_WIDTH_SMALL } from '@/constants';
+import {
+  NAVBAR_HEIGHT,
+  SIDEBAR_BREAKPOINT,
+  SIDEBAR_WIDTH_SMALL,
+} from '@/constants';
 import { lightThemeVars } from '@/styles/theme.css';
 
 export const layoutContainer = style({
@@ -22,7 +26,12 @@ export const contentArea = style({
   flex: 1,
   backgroundColor: lightThemeVars.color.white.background,
   paddingTop: NAVBAR_HEIGHT + 16,
-  paddingRight: SIDEBAR_WIDTH_SMALL + 16,
-  paddingLeft: SIDEBAR_WIDTH_SMALL + 16,
+  paddingRight: 16,
+  paddingLeft: 16,
   overflow: 'auto',
+  '@media': {
+    [`(max-width: ${SIDEBAR_BREAKPOINT}px)`]: {
+      paddingLeft: SIDEBAR_WIDTH_SMALL + 16,
+    },
+  },
 });
