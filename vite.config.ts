92ffff73d33808be805caba14fdd8daf4817747c
Seongbin Kim <seongbin9786@gmail.com>
2025-05-21 18:25:21 +0900

    chore: MAT-265 LocatorJS babel plugin 설치 및 Vite React plugin 교체

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -12,9 +12,20 @@ export default defineConfig({
   },
   plugins: [
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
-    react(),
     vanillaExtractPlugin(),
     tsconfigPaths(),
     svgr(),
+    react({
+      babel: {
+        plugins: [
+          [
+            '@locator/babel-jsx/dist',
+            {
+              env: 'development',
+            },
+          ],
+        ],
+      },
+    }),
   ],
 });
