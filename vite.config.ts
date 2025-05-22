525b699200eb30e4505a2cb761e8ed7b7f49992f
Seongbin Kim <seongbin9786@gmail.com>
2025-05-22 11:06:59 +0900

    chore: MAT-278 locatorjs babel plugin 제거, vite react plugin을 swc로 다시 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -15,17 +15,6 @@ export default defineConfig({
     vanillaExtractPlugin(),
     tsconfigPaths(),
     svgr(),
-    react({
-      babel: {
-        plugins: [
-          [
-            '@locator/babel-jsx/dist',
-            {
-              env: 'development',
-            },
-          ],
-        ],
-      },
-    }),
+    react(),
   ],
 });
