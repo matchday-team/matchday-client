e2b3e6d3d0469389e2b8b557f43d0c16b143f907
younaj00 <you.na.joo0@gmail.com>
2025-05-25 23:57:54 +0900

    feat: MAT-283 label DOM props 지원

--- src/components/CommonForm/Label/Label.tsx
+++ src/components/CommonForm/Label/Label.tsx
@@ -1,15 +1,20 @@
-import { ReactNode } from 'react';
+import { ComponentPropsWithoutRef, ReactNode } from 'react';
 
 import * as styles from './Label.css';
 
-interface LabelProps {
+interface LabelProps extends ComponentPropsWithoutRef<'label'> {
   label: string;
   children?: ReactNode;
   required?: boolean;
 }
 
-export const Label = ({ label, children, required = false }: LabelProps) => (
-  <label className={styles.textFieldLabel}>
+export const Label = ({
+  label,
+  children,
+  required = false,
+  ...rest
+}: LabelProps) => (
+  <label className={styles.textFieldLabel} {...rest}>
     {label}
     {required && <span className={styles.textFieldRequired}>*</span>}
     {children}
