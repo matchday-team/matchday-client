3d4e8e26c3b929e4640e74739367dc9cf9be63ac
Seongbin Kim <seongbin9786@gmail.com>
2025-05-25 18:35:10 +0900

    feat: MAT-269 선수 할당 기능 어댑터 구현 - 선발 그리드

---
+++ src/features/playerLineupEdit/PlayerAssignmentAdapterForStarterGrid.tsx
@@ -0,0 +1,185 @@
+import { DragEvent, ReactElement } from 'react';
+
+import { useQueryClient } from '@tanstack/react-query';
+
+import {
+  MatchUserCreateRequestRole,
+  MatchUserResponse,
+  TeamResponse,
+} from '@/apis/models';
+import {
+  useCreateMatchUserMutation,
+  useDeleteMatchUserMutation,
+} from '@/apis/mutations';
+import { matchQuery } from '@/apis/queries';
+import { useIsDragOver } from '@/hooks';
+
+import { usePlayerLineupEditStore } from './playerLineupEditStore';
+
+type RenderViewProps<Element extends HTMLElement> = (props: {
+  isDragOver: boolean;
+  disabled: boolean;
+  ref: React.Ref<Element>;
+  onDragStart: React.DragEventHandler<Element>;
+  onDragOver: React.DragEventHandler<Element>;
+  onDrop: React.DragEventHandler<Element>;
+  onDragEnd: React.DragEventHandler<Element>;
+}) => ReactElement;
+
+type PlayerAssignmentAdapterForStarterGridProps<Element extends HTMLElement> = {
+  matchId: number;
+  team: TeamResponse;
+  player: MatchUserResponse;
+  render: RenderViewProps<Element>;
+};
+
+export const PlayerAssignmentAdapterForStarterGrid = <
+  Target extends HTMLElement,
+>({
+  matchId,
+  team,
+  player,
+  render,
+}: PlayerAssignmentAdapterForStarterGridProps<Target>) => {
+  const { isDragOver, hoverTargetRef } = useIsDragOver<Target>();
+  const { selection, beginAssignment, finishAssignment } =
+    usePlayerLineupEditStore();
+
+  const queryClient = useQueryClient();
+  const { mutateAsync: createMatchUser } = useCreateMatchUserMutation();
+  const { mutateAsync: deleteMatchUser } = useDeleteMatchUserMutation();
+
+  /*
+  - 드롭이 불가능한 경우
+    - 선발 리스트 -> 선발 리스트에 드롭
+    - 팀 리스트 -> 선발 리스트에 드롭
+    - 팀 리스트 -> 선발 그리드에 드롭
+      - 골키퍼가 골키퍼 자리에 있지 않은 경우 (이거는 메시지 띄워줄만 한데? 뭐가 문젠지 알기 어려울 듯? / 뭔가 모달이 뜨면 좋을 듯)
+      - 인원수를 초과한 경우 (메시지 알려줄만 함 / 뭔가 모달이 뜨면 좋을 듯)
+    - 선발 그리드 -> 선발 그리드에 드롭
+      - 드래그/드롭한 선수 중 하나만 GK인 경우
+  */
+  // 어디서 오든 선발 그리드에 놓을 수 있다.
+  // 일단 그리드 기준으로 만듭시다
+  const isAvailable = true; // starterGrid, emptyGrid, starterList로 구분해야 함. 셋이 로직이 다름.
+
+  const handleDragStart = () => {
+    console.log('starter drag start:', player);
+
+    if (!player.matchPosition || !player.matchGrid) {
+      throw new Error('matchPosition 혹은 matchGrid가 없는 선수입니다.');
+    }
+
+    beginAssignment({
+      type: 'starter-grid',
+      player: {
+        id: player.userId,
+        teamId: team.id,
+        matchPosition: player.matchPosition,
+        matchGrid: player.matchGrid,
+        matchUserId: player.id,
+      },
+    });
+  };
+
+  const handleDragOver = (e: DragEvent<Target>) => {
+    if (!isAvailable) {
+      return;
+    }
+
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'move';
+  };
+
+  /*
+  [도착 기준]
+  - 선발 그리드 선수
+    - 팀 리스트 선수: 선발 선수 제거, 드래그한 선수를 선발에 추가(matchGrid 유지)
+    - 선발 그리드 선수: 매치유저 그리드 좌표 변경 API 호출
+    - 선발 리스트 선수: 매치유저 그리드 좌표 변경 API 호출
+    - 교체 리스트 선수: 선발 선수를 제거, 교체 선수를 제거, 선발 선수를 교체에 추가, 교체 선수를 선발에 추가(matchGrid 유지)
+  */
+  const handleDrop = async () => {
+    if (!selection || !isAvailable) {
+      return;
+    }
+    const { type, player: sourcePlayer } = selection;
+
+    switch (type) {
+      case 'all':
+        await deleteMatchUser(player.id);
+        await createMatchUser({
+          matchId,
+          userId: sourcePlayer.id,
+          teamId: sourcePlayer.teamId,
+          role: MatchUserCreateRequestRole.START_PLAYER,
+          matchPosition: sourcePlayer.matchPosition,
+          matchGrid: player.matchGrid,
+        });
+        break;
+      case 'starter-grid': // 선발 명단 - 서로 위치만 변경
+        if (typeof player.matchGrid !== 'number') {
+          throw new Error('선발 그리드 선수의 matchGrid가 number가 아님');
+        }
+        // NOTE: patchMatchUserGrid는 이미 배치된 칸에 배치할 수 없게 해놓아서 delete-create로 작업해야 함
+        await Promise.all([
+          deleteMatchUser(player.id),
+          deleteMatchUser(sourcePlayer.matchUserId),
+        ]);
+        await Promise.all([
+          createMatchUser({
+            matchId,
+            userId: sourcePlayer.id,
+            teamId: sourcePlayer.teamId,
+            role: MatchUserCreateRequestRole.START_PLAYER,
+            matchPosition: sourcePlayer.matchPosition,
+            matchGrid: player.matchGrid,
+          }),
+          createMatchUser({
+            matchId,
+            userId: player.userId,
+            teamId: team.id,
+            role: MatchUserCreateRequestRole.START_PLAYER,
+            matchPosition: player.matchPosition,
+            matchGrid: sourcePlayer.matchGrid,
+          }),
+        ]);
+        break;
+      case 'bench': // 교체 명단
+        await deleteMatchUser(player.id);
+        await deleteMatchUser(sourcePlayer.id);
+        await createMatchUser({
+          matchId,
+          userId: sourcePlayer.id,
+          teamId: sourcePlayer.teamId,
+          role: MatchUserCreateRequestRole.START_PLAYER,
+          matchPosition: sourcePlayer.matchPosition,
+          matchGrid: player.matchGrid,
+        });
+        await createMatchUser({
+          matchId,
+          userId: player.id,
+          teamId: team.id,
+          role: MatchUserCreateRequestRole.SUB_PLAYER,
+          matchPosition: player.matchPosition,
+          matchGrid: null,
+        });
+        break;
+
+      default:
+        console.warn('Unknown target mode:', type);
+    }
+    await queryClient.invalidateQueries(matchQuery.players(matchId));
+    finishAssignment();
+  };
+
+  return render({
+    isDragOver,
+    disabled: !isAvailable,
+    ref: hoverTargetRef,
+    onDragStart: handleDragStart,
+    onDragOver: handleDragOver,
+    onDrop: handleDrop,
+    onDragEnd: finishAssignment,
+  });
+};
