42be410b747bd03296a08769f6aa396dd9af5d8d
younaj00 <you.na.joo0@gmail.com>
2025-05-27 01:16:36 +0900

    refactor: MAT-294 focus 상태를 outline으로 개선하고 selectors 사용 최소화

--- src/components/CommonForm/Input/Input.css.ts
+++ src/components/CommonForm/Input/Input.css.ts
@@ -4,29 +4,30 @@ import { lightThemeVars } from '@/styles/theme.css';
 
 export const TextInput = style({
   flexShrink: 0,
-  outline: 'none',
   border: `1px solid ${lightThemeVars.color.primary[100]}`,
   borderRadius: 6,
   background: lightThemeVars.color.white.hover,
   padding: '9px 16px',
   width: 300,
   height: 38,
+  '::placeholder': {
+    lineHeight: 1.4,
+    letterSpacing: -0.35,
+    color: lightThemeVars.color.gray[300],
+    fontSize: 14,
+    fontWeight: 400,
+  },
+  ':focus': {
+    outline: `1.5px solid ${lightThemeVars.color.primary[700]}`,
+    border: `1px solid ${lightThemeVars.color.primary[700]}`,
+  },
   selectors: {
-    '&::placeholder': {
-      lineHeight: 1.4,
-      letterSpacing: -0.35,
-      color: lightThemeVars.color.gray[300],
-      fontSize: 14,
-      fontWeight: 400,
-    },
-    '&:focus': {
-      border: `1.5px solid ${lightThemeVars.color.primary[700]}`,
-    },
     '&[aria-invalid="true"]': {
       border: `1.5px solid ${lightThemeVars.color.warning}`,
     },
     '&[aria-invalid="true"]:focus': {
-      border: `1.5px solid ${lightThemeVars.color.warning}`,
+      outline: `1.5px solid ${lightThemeVars.color.warning}`,
+      border: `1px solid ${lightThemeVars.color.warning}`,
     },
   },
 });
