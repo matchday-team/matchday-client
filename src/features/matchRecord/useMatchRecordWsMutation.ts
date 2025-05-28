343a9a5481543b82116093e39e2efa6b5c339176
Seongbin Kim <seongbin9786@gmail.com>
2025-05-28 11:23:27 +0900

    refactor: MAT-304 경기 기록 웹소켓 Hook에서 읽기/쓰기 기능 분리

---
+++ src/features/matchRecord/useMatchRecordWsMutation.ts
@@ -0,0 +1,39 @@
+import { getWebSocketApi } from '@/apis/websockets';
+import { MatchEventType } from '@/constants';
+
+export const useMatchRecordWsMutation = (matchId: number) => {
+  const wsApi = getWebSocketApi();
+
+  const requestPlayerSwap = (inPlayerId: number, outPlayerId: number) => {
+    wsApi.send('recordPlayerExchange', [matchId], {
+      fromMatchUserId: outPlayerId,
+      toMatchUserId: inPlayerId,
+    });
+  };
+
+  // FIXME: 생략된 prop: isIncrement: boolean - 현재는 항상 증가만 가능
+  const requestTeamStatChange = (
+    matchEvent: MatchEventType,
+    teamId: number,
+  ) => {
+    wsApi.send('recordTeamStat', [matchId, teamId], {
+      eventType: matchEvent,
+    });
+  };
+
+  const requestPlayerStatChange = (
+    playerId: number,
+    matchEvent: MatchEventType,
+  ) => {
+    wsApi.send('recordPlayerStat', [matchId], {
+      matchUserId: playerId,
+      eventType: matchEvent,
+    });
+  };
+
+  return {
+    requestPlayerSwap,
+    requestTeamStatChange,
+    requestPlayerStatChange,
+  };
+};
