339a00f88c28f25958232d385f005faf6697aed5
Seongbin Kim <seongbin9786@gmail.com>
2025-06-03 21:07:20 +0900

    refactor: MAT-321 사이드바 패딩 조절을 페이지에서 하게 해 배경색 불일치 영역 제거

---
+++ src/styles/page.css.ts
@@ -0,0 +1,7 @@
+import { style } from '@vanilla-extract/css';
+
+import { SIDEBAR_TOGGLE_BUTTON_SIZE } from '@/constants';
+
+export const commonPageRoot = style({
+  paddingLeft: SIDEBAR_TOGGLE_BUTTON_SIZE,
+});
