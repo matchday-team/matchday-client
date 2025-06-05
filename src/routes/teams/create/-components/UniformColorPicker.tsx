002eb58eae778a1d981a3433848eac646e23f644
Seongbin Kim <seongbin9786@gmail.com>
2025-06-05 14:33:44 +0900

    feat: MAT-321 유니폼 색상 선택 임시 컴포넌트 추가

---
+++ src/routes/teams/create/-components/UniformColorPicker.tsx
@@ -0,0 +1,56 @@
+import * as styles from './UniformColorPicker.css';
+
+interface UniformColor {
+  top?: string;
+  bottom?: string;
+  socks?: string;
+}
+
+interface UniformColorPickerProps {
+  uniformColors: UniformColor;
+  onColorChange: (type: 'top' | 'bottom' | 'socks', color: string) => void;
+}
+
+const colorTypes = [
+  { key: 'top', label: '상의' },
+  { key: 'bottom', label: '하의' },
+  { key: 'socks', label: '스타킹' },
+] as const;
+
+export function UniformColorPicker({
+  uniformColors,
+  onColorChange,
+}: UniformColorPickerProps) {
+  return (
+    <div className={styles.uniformColors}>
+      {colorTypes.map(({ key, label }) => (
+        <label
+          key={`color-group-${key}`}
+          className={styles.colorGroup}
+          htmlFor={`uniform-color-${key}`}
+        >
+          <div className={styles.colorPicker}>
+            <div
+              className={styles.colorCircle({
+                isEmpty: !uniformColors?.[key],
+              })}
+              style={{
+                backgroundColor: uniformColors?.[key] || undefined,
+              }}
+            >
+              <input
+                id={`uniform-color-${key}`}
+                key={`uniform-color-${key}`}
+                type='color'
+                value={uniformColors?.[key] || '#000000'}
+                onChange={e => onColorChange(key, e.target.value)}
+                className={styles.colorInput}
+              />
+            </div>
+          </div>
+          <span className={styles.colorLabel}>{label}</span>
+        </label>
+      ))}
+    </div>
+  );
+}
