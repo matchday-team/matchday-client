d4879cc5d4fc1533352d25e8144b698551c3a2e9
Seongbin Kim <seongbin9786@gmail.com>
2025-06-05 14:14:38 +0900

    refactor: MAT-321 Input 컴포넌트가 입력 받은 className을 병합하게 개선

--- src/components/CommonForm/Input/Input.tsx
+++ src/components/CommonForm/Input/Input.tsx
@@ -1,13 +1,19 @@
 import { ComponentProps } from 'react';
 
+import clsx from 'clsx';
+
 import * as styles from './Input.css';
 
 interface InputProps extends ComponentProps<'input'> {
   isError?: boolean;
 }
 
-export function Input({ isError = false, ...props }: InputProps) {
+export function Input({ isError = false, className, ...props }: InputProps) {
   return (
-    <input aria-invalid={isError} className={styles.TextInput} {...props} />
+    <input
+      aria-invalid={isError}
+      className={clsx(styles.rootContainer, className)}
+      {...props}
+    />
   );
 }
