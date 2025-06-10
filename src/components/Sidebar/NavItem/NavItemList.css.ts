6a0a10d876e34ecbb97045e9299188c299731755
Seongbin Kim <seongbin9786@gmail.com>
2025-06-10 12:37:35 +0900

    feat: MAT-343 툴팁 - 기본 스타일로 구현

--- src/components/Sidebar/NavItem/NavItemList.css.ts
+++ src/components/Sidebar/NavItem/NavItemList.css.ts
@@ -1,3 +1,7 @@
+import { lightThemeVars } from '@/styles/theme.css';
+
+import { style } from '@vanilla-extract/css';
+
 import {
   label,
   navItem,
@@ -5,3 +9,20 @@ import {
 } from '@/components/Sidebar/SidebarCommon.css';
 
 export { navItem, navItemIcon, label };
+
+export const tooltipContent = style({
+  zIndex: 1000,
+  borderRadius: 6,
+  boxShadow: '0 4px 12px rgba(0, 0, 0, 0.15)',
+  backgroundColor: lightThemeVars.color.primary[800],
+  padding: '8px 12px',
+  maxWidth: 200,
+  whiteSpace: 'nowrap',
+  color: lightThemeVars.color.white.main,
+  fontSize: 12,
+  fontWeight: 500,
+});
+
+export const tooltipArrow = style({
+  fill: lightThemeVars.color.primary[800],
+});
