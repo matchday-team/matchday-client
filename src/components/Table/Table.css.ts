189c9e64dbe340e7d23032459cd3cbef24e8bbea
Seongbin Kim <seongbin9786@gmail.com>
2025-06-11 18:15:56 +0900

    feat: MAT-359 공통 테이블 구현

---
+++ src/components/Table/Table.css.ts
@@ -0,0 +1,63 @@
+import { lightThemeVars } from '@/styles/theme.css';
+
+import { createVar, style } from '@vanilla-extract/css';
+
+export const tableWidthVars = {
+  totalWidth: createVar(),
+};
+
+export const columnWidthVars = {
+  width: createVar(),
+};
+
+export const container = style({
+  borderRadius: 10,
+  boxShadow: '4px 4px 8px 0px rgba(0, 0, 0, 0.05)',
+  backgroundColor: lightThemeVars.color.white.main,
+  width: tableWidthVars.totalWidth,
+  overflow: 'hidden',
+});
+
+export const header = style({
+  borderTop: `1px solid ${lightThemeVars.color.primary[100]}`,
+  backgroundColor: lightThemeVars.color.white.main,
+});
+
+export const headerRow = style({
+  display: 'flex',
+  alignItems: 'center',
+  letterSpacing: -0.35,
+  color: lightThemeVars.color.gray[500],
+  fontSize: 14,
+  fontWeight: 500,
+});
+
+export const headerCell = style({
+  display: 'flex',
+  flexShrink: 0,
+  width: columnWidthVars.width,
+});
+
+export const body = style({
+  minHeight: 500,
+  overflow: 'auto',
+});
+
+export const row = style({
+  display: 'flex',
+  borderBottom: `1px solid ${lightThemeVars.color.gray[100]}`,
+  cursor: 'pointer',
+  letterSpacing: -0.4,
+  color: lightThemeVars.color.black,
+  fontSize: 16,
+
+  ':hover': {
+    backgroundColor: lightThemeVars.color.gray[100],
+  },
+});
+
+export const cell = style({
+  display: 'flex',
+  flexShrink: 0,
+  width: columnWidthVars.width,
+});
