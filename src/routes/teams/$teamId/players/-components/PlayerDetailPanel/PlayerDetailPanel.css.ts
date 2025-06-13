896e2f952583d31ce87d36e61469fde953433a9c
Seongbin Kim <seongbin9786@gmail.com>
2025-06-13 17:43:32 +0900

    feat: MAT-361 선수 상세 프로필 영역에 그림자 추가

--- src/routes/teams/$teamId/players/-components/PlayerDetailPanel/PlayerDetailPanel.css.ts
+++ src/routes/teams/$teamId/players/-components/PlayerDetailPanel/PlayerDetailPanel.css.ts
@@ -8,8 +8,9 @@ export const rootContainer = style({
   justifyContent: 'space-between',
   margin: '0 auto',
   borderRadius: 10,
+  boxShadow: '4px 4px 8px 0px rgba(0, 0, 0, 0.05)', // NOTE: 현재 디자인에 없는데 임시로 추가
   backgroundColor: lightThemeVars.color.white.main,
   width: '100%',
-  maxWidth: 480,
-  height: '100%', // 디자인에서는 100px fixed padding top
+  maxWidth: 480, // NOTE: 디자인에서는 100px fixed padding top 인데, between 확장성이 더 낫다고 보임
+  height: '100%',
 });
